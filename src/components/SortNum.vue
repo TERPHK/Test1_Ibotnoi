4a5566f512c9602bcb4dc9b21d4ea145415d0980
TERPHK <phakkharaphon23@gmail.com>
2024-09-12 00:04:25 +0700

    Initial commit

---
+++ src/components/SortNum.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <label for="number">Input </label>
+    <input type="number" v-model.number="number" id="number" />
+    <button @click="countDown">Submit</button>
+    <ul>
+      <li v-for="(n, index) in countList" :key="'countList-' + index">{{ '*'.repeat(n) }}</li>
+      <li v-for="(n, index) in countdownList" :key="'countdownList-' + index">{{ '*'.repeat(n) }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: null,
+      countList: [],
+      countdownList: [],
+    };
+  },
+  methods: {
+    countDown() {
+      this.countdownList = [];
+      this.countList = [];
+      const num = Number(this.number); // แปลงค่าที่ป้อนเป็นตัวเลข
+
+      if (num > 0) {
+        for (let i = num; i > 0; i--) {
+          this.countdownList.push(i);
+        }
+        for (let i = 1; i < num; ++i) {
+          this.countList.push(i);
+        }
+      }
+      
+      console.log('Number:', num);
+      console.log('Count List:', this.countList);
+      console.log('Countdown List:', this.countdownList);
+    }
+  }
+};
+</script>
